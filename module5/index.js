54ecee7f2ea43722b70a57e08087eb5bbb0aa75f
hoanglinhphan16 <hoanglinhphan16@gmail.com>
2024-10-21 14:24:37 +0700

    day 1

---
+++ module5/index.js
@@ -0,0 +1,54 @@
+let courses = [
+    {
+        id: 1,
+        title: "ReactJS Tutorial",
+        rating: 4.2,
+    },
+    {
+        id: 2,
+        title: "Angular Tutorial",
+        rating: 2.5,
+    },
+    {
+        id: 3,
+        title: "VueJS Tutorial",
+        rating: 3.8,
+    },
+    {
+        id: 4,
+        title: "Java Tutorial",
+        rating: 4,
+    },
+    {
+        id: 5,
+        title: "JavaScript Tutorial",
+        rating: 3.5,
+    },
+];
+
+const newCourses = courses.filter(e => e.rating >= 4);
+console.log(newCourses);
+
+const newMapCourses = newCourses.map(course => `${course.id} - ${course.title} - ${course.rating}`)
+console.log(newMapCourses);
+
+let addedCourses = [
+    {
+        id: 6,
+        title: "PHP Tutorial",
+        rating: 3,
+    },
+    {
+        id: 7,
+        title: "C# Tutorial",
+        rating: 2,
+    },
+    {
+        id: 8,
+        title: "Docker Tutorial",
+        rating: 3.8,
+    }
+];
+
+const newArray = [...courses, ...addedCourses];
+console.log(newArray);
